50d06006784bb9285ae330b4c5055c5428f43104
Christopher Strøm <detoxte@gmail.com>
2021-09-18 18:38:18 +0200

    Change old node name
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- tutorial_packages/talker_cpp/src/talker_node.cpp
+++ tutorial_packages/talker_cpp/src/talker_node.cpp
@@ -1,7 +1,7 @@
 #include "talker_cpp/talker.hpp"
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "asv_talker_cpp");
+    ros::init(argc, argv, "talker_cpp");
     ros::NodeHandle nh;
     Talker talker(nh);
 
