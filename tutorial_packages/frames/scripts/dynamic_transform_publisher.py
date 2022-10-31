ce9db26c396a21a0884922e9b136582f0acc0c66
Christopher Strøm <detoxte@gmail.com>
2022-10-31 19:33:23 +0100

    Remove kok
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- tutorial_packages/frames/scripts/dynamic_transform_publisher.py
+++ tutorial_packages/frames/scripts/dynamic_transform_publisher.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 
-
 import rospy
 
 import tf_conversions
@@ -13,8 +12,8 @@ import geometry_msgs.msg
 if __name__ == '__main__':
     rospy.init_node('tf2_broadcaster')
 
-    tf_broadcaster = tf2_ros.TransformBroadcaster()
-    transform = geometry_msgs.msg.TransformStamped()
+    tf_broadcaster = tf2_ros.TransformBroadcaster() # The broadcaster object used to publish the tf
+    transform = geometry_msgs.msg.TransformStamped() # The tf
 
     start = rospy.Time.now().to_sec()
     
@@ -24,21 +23,16 @@ if __name__ == '__main__':
         theta = time_since_start / 2.0
 
         transform.header.stamp = rospy.Time.now()
-        transform.header.frame_id = "odom"
-        transform.child_frame_id = "base_link"
 
-        
-        transform.transform.translation.x = np.cos(theta)
-        transform.transform.translation.y = np.sin(theta)
-        transform.transform.translation.z = 0.0
+        # TODO: populate the transform.header.frame_id and transform.child_frame_id according to spec
 
-        q = tf_conversions.transformations.quaternion_from_euler(0, 0, theta)
+        # TODO: populate the transform.transform.translation. fields (x, y, z)
 
-        transform.transform.rotation.x = q[0]
-        transform.transform.rotation.y = q[1]
-        transform.transform.rotation.z = q[2]
-        transform.transform.rotation.w = q[3]
+        # TODO: use the tf_conversions euler to quaternion function to generate a quaternion according to spec
 
-        tf_broadcaster.sendTransform(transform)
+        # TODO: populate the transform.transform.rotation fields (x, y, z, w) with the quaternion from last step
+
+        # TODO: publish transform
+
         rospy.sleep(0.1)
 
