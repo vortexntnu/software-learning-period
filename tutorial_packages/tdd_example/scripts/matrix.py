67780d7b317aca379fc35bd53617cc07031cc287
Christopher Strøm <chstr@stud.ntnu.no>
2023-02-12 13:14:02 +0100

    Add docstring description for Matrix init

--- tutorial_packages/tdd_example/scripts/matrix.py
+++ tutorial_packages/tdd_example/scripts/matrix.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 class Matrix:
-    def __init__(self, rows, columns, elements=None):
-        self.rows = rows
-        self.columns = columns
-        if elements is None:
-            self.elements = np.zeros((rows, columns))
-        else:
-            self.elements = np.array(elements)
+    def __init__(self, rows, columns, initial_values=None):
+        """Create a 2D matrix.
+
+        Args:
+            rows (int): number of rows
+            columns (int): number of columns
+            initial_values (list): optional parameter to set array elements on creation
+        """
 
     def __str__(self):
         return str(self.elements)
